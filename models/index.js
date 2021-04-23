d75d8a77020df555394fa29012b57b782cd2336b
Emily Wilbur <ewilbur94@gmail.com>
2021-04-22 23:19:29 -0600

    database models and server.js set up

--- models/index.js
+++ models/index.js
@@ -3,14 +3,31 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { triggerAsyncId } = require('node:async_hooks');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.belongsToMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+
+  }
+})
+
+
 
 module.exports = {
   Product,
